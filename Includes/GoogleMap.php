9f82334d17984d3a1221ce2c58438c1f48267362
Sebastian Vettel <sebastian.vettel.015@gmail.com>
2024-03-21 20:17:16 +0530

    master

---
+++ Includes/GoogleMap.php
@@ -0,0 +1,11 @@
+<?php include "./AdditionalPHP/checkAccess.php"; ?>
+
+<div class="map">
+    <div class="subtitle">
+        <h2>LOCATE US</h2>
+    </div>
+
+    <div class="google-map">
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1944.7273025353356!2d78.84247813104103!3d12.878463491183917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bad136abb3db0fb%3A0xd277509e6e6c664e!2z4K6V4K6j4K6-4K6a4K-N4K6a4K6-4K6w4K6_IOCuleCvgeCun-CvgeCuruCvjeCuquCuruCvjQ!5e0!3m2!1sen!2sin!4v1710913216840!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</div>
